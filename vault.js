ee5c6d0ea8f36fce3a6c1bfb456dabe3c8e59105
scrowley016 <33128271+scrowley016@users.noreply.github.com>
2023-05-30 20:51:20 -0400

    home page done

---
+++ vault.js
@@ -0,0 +1,15 @@
+
+
+
+const str1 = "hello "
+const str2 = "world"
+const str3 = "!"
+
+
+"hello new" + " world" + "!" + "hello new world!"
+
+str1  + str2 + str3 = "hello world!"
+2 + 4 + 6
+
+
+//hello world!
